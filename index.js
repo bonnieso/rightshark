b24d40cc0ed2b1c26bd4cfccad8c3b77149f3491
Bonnie So <bonnie_so@apple.com>
2016-08-31 17:42:02 -0700

    initializing project

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Validate an email with given specs.
+ * Default specs follow xxx@yy.yy pattern.
+ * x can be a letter, integer, or symbol (".", "_", "-"). y can be a letter or number.
+ * @param {string} email
+ * @return {boolean}
+ */
+module.exports = function(email) {
+
+  var name = email.split("@")[0].split("");
+  var address = email.split("@")[1];
+  var addressCheck = address.split(".");
+  var domain = address.split(".")[0].split("");
+  var extension = address.split(".")[1].split("");
+
+  var validName = name.every(validateWithSymbols);
+  var validDomain = domain.every(validate);
+  var validExtension = extension.every(validate);
+
+  function validate(element, index, array){
+    var valid = /^[0-9a-zA-Z]+$/;
+    return element.match(valid) ? true : false;
+  }
+
+  function validateWithSymbols(element, index, array){
+    var valid = /^[\.0-9a-zA-Z_-]+$/;
+    return element.match(valid) ? true : false;
+  }
+
+  return validName && validDomain && validExtension && addressCheck.length === 2 ? true : false;
+
+};
